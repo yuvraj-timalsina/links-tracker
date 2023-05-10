12d17203c7dfe68679b34a362886725e2d79555e
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-05-10 11:32:17 +0545

    Render function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 
@@ -7,13 +7,15 @@ const ulEl = document.getElementById("ul-el")
 console.log(ulEl)
 inputBtn.addEventListener('click', function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 })
 
-let listItems = ""
+function renderLeads() {
+    let listItems = ""
 
-for(let i=0; i<myLeads.length; i++) {
-  listItems += "<li>" + myLeads[i] + "</li>"
+    for(let i=0; i<myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+
+    ulEl.innerHTML = listItems
 }
-
-ulEl.innerHTML = listItems
