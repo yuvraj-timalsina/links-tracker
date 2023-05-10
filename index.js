35c08b594d98f86400f87fe04693da95ac99d7b0
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-05-10 11:33:48 +0545

    Clear the input

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ulEl = document.getElementById("ul-el")
 console.log(ulEl)
 inputBtn.addEventListener('click', function () {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
 
