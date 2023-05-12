192de8c0e133f5d99eee306bdf6f27417bdea285
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-05-12 13:50:08 +0545

    Local storage check before rendering leads

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById("ul-el")
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener('click', function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
